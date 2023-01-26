b006fe38788e8a71cfeb6260377f33ea47e8c9f1
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-01-26 20:38:58 -0300

    teste exception delete id - ProductServiceTests

--- backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
+++ backend/src/test/java/com/devsuperior/dscatalog/services/ProductServiceTests.java
@@ -12,6 +12,7 @@ import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import com.devsuperior.dscatalog.repositories.ProductRepository;
+import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 
 @ExtendWith(SpringExtension.class)
 public class ProductServiceTests {
@@ -32,15 +33,34 @@ public class ProductServiceTests {
 		
 		//Mockito.when(null);   //Método usando quando precisa retornar alguma coisa
 		
+		
 		Mockito.doNothing().when(repository).deleteById(existingId); //Para alguns casos que não retornam nada
 		
 		//Lança uma exceção quando passar o id que não existe.
 		Mockito.doThrow(EmptyResultDataAccessException.class).when(repository).deleteById(nonExistingId);
 	}
+	
+	
 	//Quando cria um mock, precisa simular o comportamento dele.
 	@Mock // Cria a classe de forma ilustrativa(de mentira) para fazer a simulação
 	private ProductRepository repository;
 	
+	
+	//Teste quando tenta excluit um id que não existe
+	@Test
+	public void deleteShoulResourceNotFoundExceptionWhenIdDoesNotExists() {
+			// Nesse caso não vai disparar execeção quando eu chamar o id, porque ele não existe		
+			Assertions.assertThrows(ResourceNotFoundException.class,() -> {
+				service.delete(nonExistingId);
+			});
+			
+			//mockito time verifica quantas vezes o repository foi chamado. (never segnifica que nunca deve ser chamado)
+			Mockito.verify(repository, Mockito.times(1)).deleteById(nonExistingId); // verifica se o metodo deletebyid foi chamado. 
+	}
+	
+	
+	
+	//teste para excluir um id
 	@Test
 	public void deleteShouldIdDoNothingWhenIdExists() {
 			// Nesse caso não vai disparar execeção quando eu chamar o id, porque ele não existe		
@@ -51,5 +71,7 @@ public class ProductServiceTests {
 			//mockito time verifica quantas vezes o repository foi chamado. (never segnifica que nunca deve ser chamado)
 			Mockito.verify(repository, Mockito.times(1)).deleteById(existingId); // verifica se o metodo deletebyid foi chamado. 
 	}
+	
+	
 	
 }
