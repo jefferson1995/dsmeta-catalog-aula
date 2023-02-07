3b0529ffab10a56a380136353bfc62b80558773f
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-02-07 20:05:57 -0300

    Teste de integração para findAllPaged

--- backend/src/test/java/com/devsuperior/dscatalog/resources/ProductServiceIT.java
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductServiceIT.java
@@ -5,7 +5,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.devsuperior.dscatalog.dto.ProductDTO;
 import com.devsuperior.dscatalog.repositories.ProductRepository;
 import com.devsuperior.dscatalog.services.ProductService;
 import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
@@ -13,18 +18,18 @@ import com.devsuperior.dscatalog.services.exceptions.ResourceNotFoundException;
 //Relamente acessa a classe service e verifica toda integração da aplicação com o banco de dados.
 
 @SpringBootTest
+@Transactional // Faz um rollback no banco de dados a cada teste realizado.
 public class ProductServiceIT {
 
 	@Autowired
 	private ProductService service;
 	@Autowired
 	private ProductRepository repository;
-	
+
 	private Long existingId;
 	private Long nonExistingId;
 	private Long countTotalProducts;
-	
-	
+
 	@BeforeEach
 	void setUp() throws Exception {
 
@@ -32,24 +37,60 @@ public class ProductServiceIT {
 		nonExistingId = 1000L;
 		countTotalProducts = 25L;
 	}
-	
-	//Lança um exceção quando o id não existe no banco de dados
+
+	// Testa quando a página não existe
+	@Test
+	public void findAllPagedReturnSortedOrderedPageWhenSortByName() {
+		PageRequest pageRequest = PageRequest.of(0, 10, Sort.by("name")); // Retorna uma página ordenada por nome
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+		Assertions.assertFalse(result.isEmpty()); // Verifique se a página não está vazia
+		Assertions.assertEquals("Macbook Pro", result.getContent().get(0).getName()); // Verifica se é o primeiro produto ordenado
+		Assertions.assertEquals("PC Gamer", result.getContent().get(1).getName()); // Verfica o segunto
+		Assertions.assertEquals("PC Gamer Alfa", result.getContent().get(2).getName());// terceiro
+
+	}
+
+	// Testa quando a página não existe
+	@Test
+	public void findAllPagedReturnEmptyPageDoesNotExists() {
+		PageRequest pageRequest = PageRequest.of(50, 10);
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+		Assertions.assertTrue(result.isEmpty()); // Verifique se a página não está vazia
+
+	}
+
+	// Realize o teste para verificar se está retornando uma página
+	@Test
+	public void findAllPagedReturnPage0Size10() {
+		PageRequest pageRequest = PageRequest.of(0, 10);
+		Page<ProductDTO> result = service.findAllPaged(pageRequest);
+
+		Assertions.assertFalse(result.isEmpty()); // Verifique se a página não está vazia
+		Assertions.assertEquals(0, result.getNumber()); // Verifica se a página é igual a zero
+		Assertions.assertEquals(10, result.getSize()); // Verifica se o total de páginas é 10
+		Assertions.assertEquals(countTotalProducts, result.getTotalElements()); // Verifica se tem a mesma quantidade de
+																				// itens(produtos)
+	}
+
+	// Lança um exceção quando o id não existe no banco de dados
 	@Test
 	public void deleteShouldThrowResourceNotFoundExceptionWhenIdDoesNotExists() {
-			
+
 		Assertions.assertThrows(ResourceNotFoundException.class, () -> {
-			service.delete(existingId);
+			service.delete(nonExistingId);
 		});
 	}
-	
-	//Teste para verificar se está deletando o id do banco
+
+	// Teste para verificar se está deletando o id do banco
 	@Test
 	public void deleteShouldDeleteResourceWhenIdExists() {
-		
+
 		service.delete(existingId);
-		
-		Assertions.assertEquals(countTotalProducts -1, repository.count()); // Acessa o banco de dados e verifica quantos itens tem para validar
+
+		Assertions.assertEquals(countTotalProducts - 1, repository.count()); // Acessa o banco de dados e verifica
+																				// quantos itens tem para validar
 	}
-	
-	
+
 }
