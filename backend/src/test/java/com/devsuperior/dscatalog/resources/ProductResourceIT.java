5686365568e846f7437d0c8832973ef516f83e71
jefferson barbosa dos santos <jefferson.bds@hotmail.com>
2023-02-07 20:47:57 -0300

    Teste de integração na camada web findAll

---
+++ backend/src/test/java/com/devsuperior/dscatalog/resources/ProductResourceIT.java
@@ -0,0 +1,47 @@
+package com.devsuperior.dscatalog.resources;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ProductResourceIT {
+
+	@Autowired
+	private MockMvc mockMvc; // Permite fazer as requisições
+
+	private Long existingId;
+	private Long nonExistingId;
+	private Long countTotalProducts;
+
+	@BeforeEach
+	void setUp() throws Exception {
+
+		existingId = 1L;
+		nonExistingId = 1000L;
+		countTotalProducts = 25L;
+	}
+	
+	@Test
+	public void findAllShouldReturnSortedPageWhenSortByName() throws Exception {
+		ResultActions result = mockMvc.perform(get("/products?page=0&size=12&sort=name,asc"));
+		
+		result.andExpect(status().isOk());
+		result.andExpect(jsonPath("$.totalElements").value(countTotalProducts)); //Verifica se o total de elements está correto
+		result.andExpect(jsonPath("$.content").exists()); //Verifica se existe a lista
+		result.andExpect(jsonPath("$.content[0].name").value("Macbook Pro"));
+		result.andExpect(jsonPath("$.content[1].name").value("PC Gamer"));
+		result.andExpect(jsonPath("$.content[2].name").value("PC Gamer Alfa"));//Verifica se existe o primeiro item
+	}
+}
